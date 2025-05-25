d84a1a02935ead2581c0ba654ab6e2ad52161de6
madewulf <mdewulf@bluesquarehub.com>
2025-05-25 12:18:13 +0200

    Moving the excel sheet creation to the test suite

--- api/tests.py
+++ api/tests.py
@@ -43,7 +43,9 @@ class SpreadsheetValidationTests(TestCase):
         self.create_case_insensitive_test_spreadsheet()
 
         self.create_integer_choice_test_spreadsheet()
-        
+
+        self.create_excel_date_format_spreadsheet()
+
         self.create_test_xlsform_with_aliases()
         self.create_alias_test_spreadsheet()
 
@@ -319,6 +321,29 @@ class SpreadsheetValidationTests(TestCase):
         ws.append([45, 3, "Alex Johnson", 80.2])  # Integer choice value
 
         wb.save("api/test_data/integer_choice_spreadsheet.xlsx")
+
+    def create_excel_date_format_spreadsheet(self):
+        wb = openpyxl.Workbook()
+        survey = wb.active
+        survey.title = 'survey'
+
+        survey.append(['type', 'name', 'label', 'required', 'constraint'])
+        survey.append(['date', 'last_dispensiation_date', 'Last Dispensation Date', 'yes', ''])
+        survey.append(['text', 'name', 'Name', 'yes', ''])
+
+        choices = wb.create_sheet('choices')
+        choices.append(['list_name', 'name', 'label'])
+
+        wb.save('api/test_data/test_xlsform_with_date.xlsx')
+
+        wb = openpyxl.Workbook()
+        ws = wb.active
+
+        ws.append(['last_dispensiation_date', 'name'])
+        ws.append(['2024-09-02 00:00:00', 'John Doe'])
+        ws.append(['2023-05-15 00:00:00', 'Jane Smith'])
+
+        wb.save('api/test_data/excel_date_spreadsheet.xlsx')
 
     def create_case_insensitive_test_spreadsheet(self):
         """
